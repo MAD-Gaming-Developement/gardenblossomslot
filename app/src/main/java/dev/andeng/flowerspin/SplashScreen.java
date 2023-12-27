b5d8974c0556b67a90fa5ec93e413dc2e358f51f
Andengzkii <andreagelleee@gmail.com>
2023-12-27 15:01:49 +0800

    add fb

--- app/src/main/java/dev/andeng/flowerspin/SplashScreen.java
+++ app/src/main/java/dev/andeng/flowerspin/SplashScreen.java
@@ -46,13 +46,13 @@ public class SplashScreen extends AppCompatActivity {
         RequestQueue connectAPI = Volley.newRequestQueue(this);
         JSONObject requestBody = new JSONObject();
         try {
-            requestBody.put("appid", "7T");
+            requestBody.put("appid", "W11");
             requestBody.put("package", getPackageName());
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
-        String endPoint = "https://backend.madgamingdev.com/api/gameid" + "?appid=7T&package=" + getPackageName();
+        String endPoint = "https://backend.madgamingdev.com/api/gameid" + "?appid=W11&package=" + getPackageName();
 
         JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, endPoint, requestBody,
                 response -> {
